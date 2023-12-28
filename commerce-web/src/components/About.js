b4b302d0d385adee889d92351cae1065f61390c5
Larissa Cruz <larissacruz_01@hotmail.com>
2023-12-28 12:39:49 -0300

    alterando About e Home

--- commerce-web/src/components/About.js
+++ commerce-web/src/components/About.js
@@ -1,9 +1,8 @@
 function About() {
     return (
         <div>
-            <p>
-                Esta é a página ABOUT!
-            </p>
+            <p>Trata-se de um sistema de CRUD de produtos.</p>
+            <p>O usuário administrador, previamente cadastrado na plataforma, poderá cadastrar, alterar, deletar, bem como, ver todos os produtos cadastrados -e presentes- na base de dados.</p>
         </div>
     )
 }
